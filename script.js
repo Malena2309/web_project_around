6e58061e5883f70f8b7a4e1d6f4235ab70e2850c
christian garcia <christian.garcia@labbe.io>
2023-11-27 20:57:40 -0600

    ajustes sesion 1 a 1

--- script.js
+++ script.js
@@ -1,41 +1,28 @@
-let container=document.querySelector('.container');
-let namesContainer=container.querySelector ('names-container');
-let names=namesContainer.querySelectorAll('.name');
-let addButton=container.querySelector(".form__submit-bt_action_add");
-let resetButton=container.querySelector(".form__submit-btn_action_reset");
-const openFormButton = document.querySelector('.button_type_submit');
-const popup =document.querySelector('.popup');
+let addButton = document.querySelector(".form__submit-bt_action_add");
+const openFormButton = document.querySelector('.button_type_edit-profile');
+const popup = document.querySelector('.popup');
 const closeButton = popup.querySelector('.popup__close-button');
-function toggleForm () {
+function toggleForm() {
     popup.classList.toggle('popup_visible');
 }
 
 openFormButton.addEventListener('click', toggleForm);
+
 closeButton.addEventListener('click', toggleForm);
 const form = document.querySelector('.popup__form');
-function handleFormSubmit() {
-    form.addEventListener('submit', handleFormSubmit);
-}
-const form = document.querySelector('.popup__form');
-function handleFormSubmit(event) {
-    event.preventDefault();
-    form.addEventListener('submit', handleFormSubmit);
-}
-
-const profileName = document.querySelector('.profile__name');
-const profileJob = document.querySelector('.profile__job');
-
-const input Name = document.querySelector('.popup__input_type_name');
-const input Job = document.querySelector('.popup__input_type_job');
 
 function handleFormSubmit(event) {
     event.preventDefault();
     profileName.textContent = inputName.value;
     profileJob.textContent = inputJob.value;
+    toggleForm();
 }
 
-function toggleForm () {
-    inputName.value = profileName.textContent;
-    inputBreed.value = profileJob.textContent;
-    popup.classList.add('.popup_visible');
-}
+form.addEventListener('submit', handleFormSubmit);
+
+const profileName = document.querySelector('.profile__name');
+const profileJob = document.querySelector('.profile__job');
+
+const inputName = document.querySelector('.popup__input_type_name');
+const inputJob = document.querySelector('.popup__input_type_job');
+

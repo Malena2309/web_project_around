052a1dd548fccfbb2d8f86a058d860c86e9bb363
Maria Elena Rodriguez S <mers74@gmail.com>
2023-11-29 19:43:58 -0600

    subo cambios en around

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
+//variables
 let addButton = document.querySelector(".form__submit-bt_action_add");
 const openFormButton = document.querySelector('.button_type_edit-profile');
 const popup = document.querySelector('.popup');
 const closeButton = popup.querySelector('.popup__close-button');
+const profileName = document.querySelector('.profile__name');
+const profileJob = document.querySelector('.profile__job');
+const inputName = document.querySelector('.popup__input_type_name');
+const inputJob = document.querySelector('.popup__input_type_job');
+
 function toggleForm() {
     popup.classList.toggle('popup_visible');
 }
@@ -15,14 +21,11 @@ function handleFormSubmit(event) {
     event.preventDefault();
     profileName.textContent = inputName.value;
     profileJob.textContent = inputJob.value;
+    function.reset();
     toggleForm();
 }
 
 form.addEventListener('submit', handleFormSubmit);
 
-const profileName = document.querySelector('.profile__name');
-const profileJob = document.querySelector('.profile__job');
 
-const inputName = document.querySelector('.popup__input_type_name');
-const inputJob = document.querySelector('.popup__input_type_job');
 

d9a92a59e78a0c82a42408cf37dff92d427202ec
Maria Elena Rodriguez S <mers74@gmail.com>
2023-11-29 23:46:07 -0600

    subo correcciones de around

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 //variables
-let addButton = document.querySelector(".form__submit-bt_action_add");
+const addButton = document.querySelector(".button_type_edit-profile");
 const openFormButton = document.querySelector('.button_type_edit-profile');
 const popup = document.querySelector('.popup');
 const closeButton = popup.querySelector('.popup__close-button');
@@ -7,6 +7,7 @@ const profileName = document.querySelector('.profile__name');
 const profileJob = document.querySelector('.profile__job');
 const inputName = document.querySelector('.popup__input_type_name');
 const inputJob = document.querySelector('.popup__input_type_job');
+const form = document.querySelector('.popup__form');
 
 function toggleForm() {
     popup.classList.toggle('popup_visible');
@@ -15,13 +16,13 @@ function toggleForm() {
 openFormButton.addEventListener('click', toggleForm);
 
 closeButton.addEventListener('click', toggleForm);
-const form = document.querySelector('.popup__form');
+
 
 function handleFormSubmit(event) {
     event.preventDefault();
     profileName.textContent = inputName.value;
     profileJob.textContent = inputJob.value;
-    function.reset();
+    form.reset();
     toggleForm();
 }
 
